3e129e9cb7d6bde46d972b60995553bf7a208440
Rupesh Salwe <rupeshs.097@gmail.com>
2026-05-21 18:09:01 +0530

    fix: Correct typo and syntax in 55_Only_if.js - standalone else is invalid

--- chapter_07_If_else/55_Only_if.js
+++ chapter_07_If_else/55_Only_if.js
@@ -1,10 +1,10 @@
-//Only(Single) if is allwoed in javascript
+//***Only (Single) if is allowed in javascript***
 if (true) {
     console.log("Only if is allowed");
 }
 
-//***Only (Single) else is not allowed in javascript***
-
-else {
-    console.log("Only else is Not allowed");
-}
+//***Only (Single) else is NOT allowed in javascript - it will throw a syntax error***
+// Below code is commented out because standalone 'else' without 'if' is invalid:
+// else {
+//     console.log("Only else is Not allowed");
+// }
