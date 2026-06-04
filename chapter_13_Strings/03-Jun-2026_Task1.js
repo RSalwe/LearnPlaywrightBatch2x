ccd0398ba9ee6c46e37269d9e86c4b0716c8026b
Rupesh <rupeshs.097@gmail.com>
2026-06-05 01:04:47 +0530

    Add chapter 13 string tasks - 03-Jun-2026

---
+++ chapter_13_Strings/03-Jun-2026_Task1.js
@@ -0,0 +1,20 @@
+function checkPalindrome(str) {
+
+    //creating an empty var to store reverse string
+    var reversedStr = "";
+    //reversing the entered string
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+
+    }
+    //optional to print reversed string
+    console.log("Reversed: " + reversedStr);
+
+    //comparing the given and reversed string
+    const match = reversedStr.toLowerCase() === str.toLowerCase();
+    console.log(match);
+    return match;
+}
+checkPalindrome("Madam");
+checkPalindrome("pramod");
+checkPalindrome("racecar");
