85d5bcaa6f5756456ef726c721f1eccbc95f25c9
Rupesh <rupeshs.097@gmail.com>
2026-06-28 13:27:52 +0530

    Add Chapter 20 -- TypeScript Basics
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ chapter_20_Typescript_Basics/01_EXPORT IMPORT/169_Utils.js
@@ -0,0 +1,7 @@
+import { BASE_URL as bul_util, formatTestName } from "../utils.js";
+import { BASE_URL as bul_testutil, formatUpperCaseString } from "../testutils.js";
+
+console.log(bul_util);
+console.log(bul_testutil);
+console.log(formatUpperCaseString("login"));
+
---
+++ chapter_20_Typescript_Basics/logger.js
@@ -0,0 +1,6 @@
+export default function log(message) {
+    console.log("[LOG] - default " + message);
+}
+export function log2(message) {
+    console.log("[LOG] " + message);
+}
